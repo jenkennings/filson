6825819f62d4c059eecc56d29a9b25bf095f24f1
bc <bc@bcmachine.localdomain>
2026-06-09 15:32:51 -0700

    rule4: split filson_expand_globs via eg_grow/eg_add_glob helpers

--- globbing.c
+++ globbing.c
@@ -49,74 +49,68 @@ filson_glob_expand(const char *pattern, glob_t *pglob)
 	return filson_glob_expand_impl(pattern, pglob);
 }
 
+static char **
+filson_eg_grow(char **expanded, int *bufsize_p)
+{
+	char **temp;
+	*bufsize_p += FILSON_GLOB_BUFSIZE;
+	temp = realloc(expanded, *bufsize_p * sizeof(char *));
+	if (!temp) { fprintf(stderr, "filson: allocation error\n"); }
+	return temp;
+}
+
+static int
+filson_eg_add_glob(char **pglob_pathv, int pathc, char **expanded,
+    int *position_p, int *bufsize_p, char ***expanded_p)
+{
+	int j;
+	for (j = 0; j < pathc; j++) {
+		if (*position_p >= *bufsize_p - 1) {
+			char **t = filson_eg_grow(*expanded_p, bufsize_p);
+			if (!t) return 0;
+			*expanded_p = t;
+			expanded = t;
+		}
+		expanded[*position_p] = malloc(strlen(pglob_pathv[j]) + 1);
+		if (!expanded[*position_p]) { fprintf(stderr, "filson: allocation error\n"); return 0; }
+		strcpy(expanded[*position_p], pglob_pathv[j]);
+		(*position_p)++;
+	}
+	return 1;
+}
+
 char **
 filson_expand_globs(char **args)
 {
-	int i, j, has_globs;
+	int i, has_globs, bufsize, position;
 	glob_t pglob;
-	char **expanded, **temp;
-	int bufsize, position;
+	char **expanded;
 
-	if (args == NULL) {
-		return NULL;
-	}
+	if (args == NULL) return NULL;
 	has_globs = 0;
-	for (i = 0; args[i] != NULL; i++) {
-		if (filson_has_glob_chars_impl(args[i])) {
-			has_globs = 1;
-			break;
-		}
-	}
-	if (!has_globs) {
-		return args;
-	}
+	for (i = 0; args[i] != NULL; i++)
+		if (filson_has_glob_chars_impl(args[i])) { has_globs = 1; break; }
+	if (!has_globs) return args;
 	bufsize = FILSON_GLOB_BUFSIZE;
 	position = 0;
 	expanded = malloc(bufsize * sizeof(char *));
-	if (!expanded) {
-		fprintf(stderr, "filson: allocation error\n");
-		return args;
-	}
+	if (!expanded) { fprintf(stderr, "filson: allocation error\n"); return args; }
 	for (i = 0; args[i] != NULL; i++) {
 		if (filson_has_glob_chars_impl(args[i])) {
 			if (filson_glob_expand_impl(args[i], &pglob) == 0 && pglob.gl_pathc > 0) {
-				for (j = 0; j < (int)pglob.gl_pathc; j++) {
-					if (position >= bufsize - 1) {
-						bufsize += FILSON_GLOB_BUFSIZE;
-						temp = realloc(expanded, bufsize * sizeof(char *));
-						if (!temp) {
-							fprintf(stderr, "filson: allocation error\n");
-							globfree(&pglob);
-							free(expanded);
-							return args;
-						}
-						expanded = temp;
-					}
-					expanded[position] = malloc(strlen(pglob.gl_pathv[j]) + 1);
-					if (!expanded[position]) {
-						fprintf(stderr, "filson: allocation error\n");
-						globfree(&pglob);
-						free(expanded);
-						return args;
-					}
-					strcpy(expanded[position], pglob.gl_pathv[j]);
-					position++;
+				if (!filson_eg_add_glob(pglob.gl_pathv, (int)pglob.gl_pathc,
+				    expanded, &position, &bufsize, &expanded)) {
+					globfree(&pglob); free(expanded); return args;
 				}
 				globfree(&pglob);
 			}
 		} else {
 			if (position >= bufsize - 1) {
-				bufsize += FILSON_GLOB_BUFSIZE;
-				temp = realloc(expanded, bufsize * sizeof(char *));
-				if (!temp) {
-					fprintf(stderr, "filson: allocation error\n");
-					free(expanded);
-					return args;
-				}
-				expanded = temp;
+				char **t = filson_eg_grow(expanded, &bufsize);
+				if (!t) { free(expanded); return args; }
+				expanded = t;
 			}
-			expanded[position] = args[i];
-			position++;
+			expanded[position++] = args[i];
 		}
 	}
 	expanded[position] = NULL;
